c569a7cf043105a37c7826e290c9b5186abaeea5
Vinicius Espindola <vinnyaoe@gmail.com>
2021-01-06 20:37:39 +0000

    fix axios default  address root api

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // http://localhost:3000
 
 const api = axios.create({
-  baseURL: process.env.VUE_APP_BASE_ROOT_API,
+  baseURL: process.env.VUE_APP_BASE_ROOT_API || 'localhost:8000',
 });
 api.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 api.defaults.headers.common['Content-Type'] = 'Application/json';
